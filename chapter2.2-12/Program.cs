4348712d2122e65f4f0210b9b4eb0f81b6bd73a7
username <1151555861@qq.com>
2021-06-30 15:17:31 +0800

    first commit

---
+++ chapter2.2-12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace chapter2._2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> enumerable = CreateSimpleIterator();
+            static IEnumerable<int> CreateSimpleIterator()
+            {
+                yield return 10;
+                for (int i = 0; i < 3; i++)
+                {
+                    yield return i;
+                }
+                yield return 20;
+            }
+            using (IEnumerator<int> enumerator =
+                enumerable.GetEnumerator())
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        int value = enumerator.Current;
+                        Console.WriteLine(value);
+                    }
+                }
+                Console.ReadKey();
+        }
+    }
+}
